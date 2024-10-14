19d6829ae341aae227dd4697b41ccca629b8493d
Racem-BOUCHNAK <140710547+Racem-BOUCHNAK@users.noreply.github.com>
2024-10-14 17:40:26 +0100

    0x01

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async """
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """spawn wait_random n times with the specified max_delay
+    """
+
+    delays = [task_wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
